381d68615bb4a4ac09c96d7ead458e7a6517ff3e
Daniel <daniel.reverol@gmail.com>
2019-03-13 18:03:25 -0400

    first commit

---
+++ src/components/Frontpage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Header from './general/Header'
+
+export default class Frontpage extends Component {
+  render() {
+    return (
+      <div>
+      <Header />
+      </div>
+    )
+  }
+}
